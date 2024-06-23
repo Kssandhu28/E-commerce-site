240bff542d86112ccc94cbde6262a6a78199c0cd
Kssandhu28 <kssandhu0120@gmail.com>
2024-06-23 00:52:43 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
@@ -16,4 +17,24 @@ if (close) {
     
 }
 
+=======
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active');
+    })
+    
+}
+
+if (close) {
+    close.addEventListener('click', () =>{
+        nav.classList.remove('active');
+    })
+    
+}
+
+>>>>>>> e47061607e7517b81fc8818411af2cc5e8aa9008
  
